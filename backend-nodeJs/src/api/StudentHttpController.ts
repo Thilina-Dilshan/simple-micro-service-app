99581c0c1689dccb61327a44b610538ba8e49dc5
Thilina Dilshan <pathiranagedilshan@gmail.com>
2023-07-03 19:35:58 +0530

    Add API layer

--- backend-nodeJs/src/api/StudentHttpController.ts
+++ backend-nodeJs/src/api/StudentHttpController.ts
@@ -1,3 +1,22 @@
 import express from "express";
 
 export const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.send("<h1>Get all students</h1>")
+});
+
+router.delete('/:id', (req, res) => {
+    res.send("<h1>Delete a students</h1>")
+
+});
+
+router.patch('/:id', (req, res) => {
+    res.send("<h1>Update a students</h1>")
+
+});
+
+
+
+
+
--- backend-nodeJs/src/main.ts
+++ backend-nodeJs/src/main.ts
@@ -6,4 +6,3 @@ let app = express();
 app.use("/api/v1/students", StudentHttpController);
 app.listen(8081, () => console.log("Server started from 8081"));
 
-
